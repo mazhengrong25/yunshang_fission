88b9b4fb90970977d255538fda770c097fe8dc47
wishQAQ <wishtime1024@gmail.com>
2020-08-11 13:47:17 +0800

    同步请求token

--- api/request.js
+++ api/request.js
@@ -2,40 +2,42 @@
  * @Description: 封装uniapp request
  * @Author: wish.WuJunLong
  * @Date: 2020-07-20 18:36:20
- * @LastEditTime: 2020-08-06 09:50:38
+ * @LastEditTime: 2020-08-11 13:46:40
  * @LastEditors: wish.WuJunLong
  */
 
 let loginInfo = uni.getStorageSync("loginInfo");
-if (loginInfo) {
-  let currentTime = new Date();
-  let loginTime = new Date(loginInfo.loginTime);
-  if (new Date(currentTime).getTime() > new Date(loginTime).getTime()) {
-    uni.request({
-      method: "POST",
-      url: "http://192.168.0.187:8092/api/login",
-      data: {
-        login_name: loginInfo.account,
-        password: loginInfo.password,
-      },
-      success: (res) => {
-        if (res.data.errorcode === 10000) {
-          let loginInfo = {
-            account: uni.getStorageSync("loginInfo").account,
-            password: uni.getStorageSync("loginInfo").password,
-            token: res.data.data.access_token,
-            loginTime: new Date(
-              new Date().getTime() + (3600 * 1000)
-            ),
-          };
-          uni.setStorageSync("loginInfo", loginInfo);
-        }
-      },
-    });
+function getToken() {
+  if (loginInfo) {
+    let currentTime = new Date();
+    let loginTime = new Date(loginInfo.loginTime);
+    if (new Date(currentTime).getTime() > new Date(loginTime).getTime()) {
+      uni.request({
+        method: "POST",
+        url: "http://192.168.0.187:8092/api/login",
+        data: {
+          login_name: loginInfo.account,
+          password: loginInfo.password,
+        },
+        success: (res) => {
+          if (res.data.errorcode === 10000) {
+            let loginInfo = {
+              account: uni.getStorageSync("loginInfo").account,
+              password: uni.getStorageSync("loginInfo").password,
+              token: res.data.data.access_token,
+              loginTime: new Date(
+                new Date().getTime() + (3600 * 1000)
+              ),
+            };
+            uni.setStorageSync("loginInfo", loginInfo);
+          }
+        },
+      });
+    }
   }
 }
 
-const request = (config, type) => {
+const request = async (config, type) => {
   uni.showLoading({
     title: "加载中",
   });
@@ -57,7 +59,10 @@ const request = (config, type) => {
   if (!config.data) {
     config.data = {};
   }
-  let promise = new Promise(function (resolve, reject) {
+
+  await getToken()
+
+  let promise = await new Promise(function (resolve, reject) {
     uni
       .request(config)
       .then((responses) => {
